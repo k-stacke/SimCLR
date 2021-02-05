0eaf3320729920c1e4418bb2abc682d46a5356af
Karin Stacke <karin.stacke@sectra.com>
2021-02-05 11:46:17 +0100

    Use normal resnet instead

--- simclr/model.py
+++ simclr/model.py
@@ -4,19 +4,27 @@ import torch.nn.functional as F
 from torch.cuda import amp
 from torchvision.models import resnet50
 
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+    
+    def forward(self, x):
+        return x
 
 class Model(nn.Module):
     def __init__(self, feature_dim=128):
         super(Model, self).__init__()
 
-        self.f = []
-        for name, module in resnet50(pretrained=False).named_children():
-            if name == 'conv1':
-                module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-            if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
-                self.f.append(module)
-        # encoder
-        self.f = nn.Sequential(*self.f)
+        #self.f = []
+        #for name, module in resnet50(pretrained=False).named_children():
+        #    if name == 'conv1':
+        #        module = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        #    if not isinstance(module, nn.Linear) and not isinstance(module, nn.MaxPool2d):
+        #        self.f.append(module)
+        ## encoder
+        #self.f = nn.Sequential(*self.f)
+        self.f = resnet50(pretrained=False)
+        self.f.fc = Identity()
         # projection head
         self.g = nn.Sequential(nn.Linear(2048, 512, bias=False),
                                nn.BatchNorm1d(512),
